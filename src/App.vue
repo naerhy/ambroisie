38e43f1e17c21c342f53509116b52322af5703fd
Naerhy <naerhy@protonmail.com>
2024-06-04 00:11:20 +0200

    Remove unused files and format code with Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <h1>Ambroisie</h1>
+  </header>
+  <main></main>
+  <footer>
+    <p>
+      <small>
+        L'ambroisie est une substance divine de la mythologie grecque, nourriture délicieuse des
+        dieux qui leur assure avec le nectar leur immortalité.
+      </small>
+    </p>
+  </footer>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+footer {
+  text-align: center;
 }
 </style>
