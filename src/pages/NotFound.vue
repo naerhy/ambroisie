ecbfdebbb204b36440e2502c8de0f6e6ee350276
Naerhy <naerhy@protonmail.com>
2024-06-21 22:15:10 +0200

    Add minimal 404 route

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+</script>
+
+<template>
+  <h2>Not found</h2>
+  <button type="button" @click="router.push(`/`)">Retourner à l'accueil</button>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import HomePage from "./pages/HomePage.vue";
 import MealsPage from "./pages/MealsPage.vue";
 import LoginPage from "./pages/LoginPage.vue";
 import AdminPage from "./pages/AdminPage.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/repas", component: MealsPage },
   { path: "/connexion", component: LoginPage },
-  { path: "/admin", component: AdminPage }
+  { path: "/admin", component: AdminPage },
+  { path: "/:pathMatch(.*)*", component: NotFound }
 ];
 
 // TODO: modify router's type [?]
