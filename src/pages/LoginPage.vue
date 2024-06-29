e3d51734dfab2c2498af7cf661c2bd0708af48bd
Naerhy <naerhy@protonmail.com>
2024-06-29 14:05:04 +0200

    Improve style of login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import axios from "axios";
 import Cookies from "js-cookie";
 import { authStore } from "../stores";
 import { useRouter } from "vue-router";
+import { useAxios } from "../composables";
 
 const router = useRouter();
+
+const { errMsg, fetch } = useAxios();
 
 const password = ref("");
 
 async function handleSubmit() {
-  try {
-    const res = await axios.post<{ token: string }>("https://naerhy.ovh/ambroisie/auth", {
+  const data = await fetch<{ token: string }>({
+    method: "POST",
+    url: "https://naerhy.ovh/ambroisie/auth",
+    data: {
       password: password.value
-    });
-    Cookies.set("token", res.data.token, { expires: 7 }); // TODO: add secure + sameSite
-    authStore.updateToken(res.data.token);
+    }
+  });
+  if (data) {
+    Cookies.set("token", data.token, { expires: 7 }); // TODO: add secure + sameSite
+    authStore.updateToken(data.token);
     router.push("/");
-  } catch (err) {
-    console.error(err);
+  } else {
+    password.value = "";
   }
 }
 </script>
@@ -27,11 +33,36 @@ async function handleSubmit() {
   <section>
     <h2>Accès à l'espace administrateur</h2>
     <form @submit.prevent="handleSubmit">
-      <label>
+      <label class="form-control">
         Mot de passe
-        <input class="input" type="text" v-model="password" />
+        <input
+          class="input"
+          type="password"
+          v-model="password"
+          :aria-invalid="errMsg ? 'true' : 'false'"
+          required
+        />
+        <small v-if="errMsg" class="text-error">{{ errMsg }}</small>
       </label>
       <button class="btn">Connexion</button>
     </form>
   </section>
 </template>
+
+<style>
+section {
+  margin: 4rem auto 0 auto;
+  max-width: 600px;
+}
+
+h2 {
+  margin-bottom: 2rem;
+  text-align: center;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+}
+</style>
