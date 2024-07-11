6bff499aa7c60c9dbe9f8cedf9416179e7895fb5
Naerhy <naerhy@protonmail.com>
2024-07-11 23:44:40 +0200

    Improve layout and style of page 404

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
 import { useRouter } from "vue-router";
+import Illustration404 from "../components/SVG/Illustration404.vue";
 
 const router = useRouter();
 </script>
 
 <template>
-  <h2>Not found</h2>
-  <button type="button" @click="router.push(`/`)">Retourner à l'accueil</button>
+  <section>
+    <h2>Page introuvable</h2>
+    <Illustration404 />
+    <button type="button" class="btn" @click="router.push(`/`)">Retourner à l'accueil</button>
+  </section>
 </template>
+
+<style scoped>
+section {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  max-width: 42rem;
+  row-gap: 2rem;
+}
+</style>
